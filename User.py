f1d91e8acdd51639c742b0c1ed64eeb2393bc436
Jacob Hill <hilljake93@gmail.com>
2022-05-31 14:32:20 -0500

    first commit

--- User.py
+++ User.py
@@ -1,1 +1,14 @@
-# your User class goes here
+class User:
+    def __init__(self, name, email, id_number, major, year_born):
+        self.name = name
+        self.email = email
+        self.id_number = id_number
+        self.major = major
+        self.year_born = year_born
+
+jake = User('Jake','hilljake93@gmail.com',1234567,'biology',1993)
+kate = User('Kate','katehill734@gmail.com',7654321,'education',1992)
+becca = User('Becca','becmelang@gmail.com',9876543,'anthropology',1989)
+frank = User('Frank','frobenalt@gmail.com',9361836,'political science',1991)
+jesse = User('Jesse','jbfern@gmail.com',1902944,'criminal justice',1992)
+rachel = User('Rachel','rachgrs@gmail.com',1928384,'marketing',1991)
